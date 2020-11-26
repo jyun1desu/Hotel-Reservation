181c29e95efbc235ac1517247cae8cf70471cd75
jyun1 <jyuny12345@gmail.com>
2020-11-26 19:06:02 +0800

    完成logo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,67 @@
 <template>
-  <div id="home">
-    <div class="room_background"></div>
-  </div>
+    <img src="./assets/room_example.jpeg" class="room_background">
+    <div class="logo">
+      <div class="shadow"></div>
+      <div class="brand_box">
+        <h1 class="brand_name">White Space</h1>
+      </div>
+    </div>
 </template>
 
 <style lang="scss">
-body{
+html,body,#app{
+  width: 100%;
+  height: 100%;
   margin: 0;
+  font-family: 'Open Sans', sans-serif;
 }
-#home{
-  background-color: pink;
+.room_background{
+  position: fixed;
+  top:0;
+  left:0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: -1;
 }
+
+.logo{
+  user-select: none;
+  display: inline-block;
+  position: relative;
+  margin-top: 50px;
+  margin-left: 50px;
+
+  .shadow{
+    width: 150px;
+    height: 150px;
+    clip-path: polygon(0% 0%, 0% 100%, 10% 100%, 10% 10%, 100% 10%, 100% 0%);
+    background-image: repeating-linear-gradient(45deg, 
+    white 10px,
+    white 11px,
+    rgba(255, 255, 255, 0.3) 11px,
+    rgba(255, 255, 255, 0.3) 16px);
+  }
+  .brand_box{
+    position: absolute;
+    left: 14px;
+    top: 14px;
+    width: 150px;
+    height: 150px;
+    border: 2px solid white;
+    background-color: rgba(255, 255, 255, 0.3);
+    display: flex;
+    align-items: center;
+      .brand_name{
+      margin: 0;
+      font-size: 30px;
+      font-weight: 400;
+      color: white;
+      letter-spacing: 2px;
+      text-align: center;
+      }
+  }
+}
+
+
 </style>
