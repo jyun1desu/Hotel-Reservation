3040ffd7867e816a32129f3f4c21b21a2f658079
jyun1 <jyuny12345@gmail.com>
2020-11-27 14:17:35 +0800

    room page banner add

--- src/views/RoomPage.vue
+++ src/views/RoomPage.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="room_page">
+    <div class="banner">
+      <div class="main_image">
+        <img src="../assets/room_example.jpeg" alt="" />
+      </div>
+      <div class="sub_images">
+        <img src="../assets/example2.jpeg" alt="" />
+        <img src="../assets/example3.jpeg" alt="" />
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.banner {
+  width: 100%;
+  height: 80vh;
+  display: flex;
+
+  .main_image {
+    flex-basis: 65%;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+
+  .sub_images {
+    flex-basis: 35%;
+    display: flex;
+    flex-direction: column;
+    img {
+      display: block;
+      width: 100%;
+      height: 50%;
+      object-fit: cover;
+
+    }
+  }
+}
+</style>
