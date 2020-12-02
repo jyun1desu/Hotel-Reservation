387d8a720b96612d63e215509c7d9fd16af85726
jyun1 <jyuny12345@gmail.com>
2020-12-02 10:54:15 +0800

    api (axios) instance created

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const token = 'DnIzKEiHHdIYCLTBo6i66cNBIPQPg8MUBwTjCsWaSX9yHJWz94z6frRgQecq';
 
 const hotelAPI = axios.create({
-    baseURL: 'https://challenge.thef2e.com/api/thef2e2019/stage6/';
+    baseURL: 'https://challenge.thef2e.com/api/thef2e2019/stage6/',
     headers:{
         'Content-Type':'application/json',
         Accept:'application/json',
